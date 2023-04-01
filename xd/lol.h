05f544ef114fa86c85a7bbc1a83603193461f415
aaron <TG4LAaron@users.noreply.github.com>
2023-04-02 00:01:02 +0100

    Create lol.h

---
+++ xd/lol.h
@@ -0,0 +1,22 @@
+#pragma once
+
+/**
+ * @brief description
+ *
+ * @author Matthew Freeman
+ */
+class Lockable {
+    private:
+    public:
+        /**
+         * @brief Destroy
+         *
+         */
+        ~Lockable(){};
+
+        /**
+         * @brief Construct
+         *
+         */
+        Lockable(){};
+};
